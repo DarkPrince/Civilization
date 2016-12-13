d5dc8dca01e707108493d30146018574117c6091
DarkPrince <zakhryapin.vs@yandex.ru>
2016-12-13 14:07:05 +0400

    Updage 0.1
    FULL UPDAGE

---
+++ src/main/java/ru/svtz/cilivization/ClientProxy.java
@@ -0,0 +1,9 @@
+package ru.svtz.cilivization;
+
+
+public class ClientProxy extends CommonProxy {
+ 
+    public void init() {
+ 
+    }
+}
---
+++ src/main/java/ru/svtz/cilivization/CommonProxy.java
@@ -0,0 +1,9 @@
+package ru.svtz.cilivization;
+
+public class CommonProxy {
+ 
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+}
